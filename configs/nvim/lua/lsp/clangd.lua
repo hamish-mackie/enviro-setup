c0bd916d03f5a89921d7356932d7bc541eb1194a
Hamish <hamish.g.mackie@gmail.com>
2025-05-22 11:26:09 +0000

    fix lsp servers

--- configs/nvim/lua/lsp/clangd.lua
+++ configs/nvim/lua/lsp/clangd.lua
@@ -1,11 +1,12 @@
-local lspconfig = require("lspconfig")
-
-lspconfig.clangd.setup({
-	cmd = {
-		"clangd",
-		"--clang-tidy",
-		"--completion-style=detailed",
-		"--header-insertion=iwyu",
-		"--clang-tidy-checks=-*,performance-*,modernize-*,readability-*,clang-analyzer-*",
-	},
-})
+return {
+    cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--completion-style=detailed",
+        "--header-insertion=iwyu",
+        "--clang-tidy-checks=-*,performance-*,modernize-*,readability-*,clang-analyzer-*",
+    },
+    root_markers = { "compile_commands.json", "compile_flags.txt" },
+    filetypes = { "c", "cpp", "h", "hpp" },
+}
