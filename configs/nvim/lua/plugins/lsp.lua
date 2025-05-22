55202e91a3a8df40c670c6e593357a8164cb5b14
Hamish <hamish.g.mackie@gmail.com>
2025-05-22 11:13:48 +0000

    fix lsp servers

--- configs/nvim/lua/plugins/lsp.lua
+++ configs/nvim/lua/plugins/lsp.lua
@@ -1,14 +1,14 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = { "b0o/schemastore.nvim" },
-		config = function()
-			require("lsp.clangd")
-			require("lsp.pyright")
-			require("lsp.ansible")
-			require("lsp.json")
-			require("lsp.yaml")
-			require("lsp.lua_ls")
-		end,
-	},
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = { "b0o/schemastore.nvim" },
+        config = function()
+            require("lsp.clangd")
+            require("lsp.pyright")
+            require("lsp.ansiblels")
+            require("lsp.jsonls")
+            require("lsp.yamlls")
+            require("lsp.lua_ls")
+        end,
+    },
 }
--- configs/nvim/lua/plugins/mason.lua
+++ configs/nvim/lua/plugins/mason.lua
@@ -1,8 +1,25 @@
--- mason.lua
+-- lua/plugins/mason.lua
 return {
-  "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end,
+  {
+    -- make sure you’ve got the correct repo here:
+    "williamboman/mason.nvim",
+    opts = {},  -- any Mason UI settings you like
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      ensure_installed   = { "lua_ls", "clangd", "pyright", "yamlls", "jsonls", "ansiblels" },
+      automatic_installation = true,  -- install on startup if missing
+    },
+    config = function(_, opts)
+      require("mason").setup()
+      require("mason-lspconfig").setup(opts)
+      local lspconfig = require("lspconfig")
+      for _, srv in ipairs(opts.ensure_installed) do
+        lspconfig[srv].setup({})
+      end
+    end,
+  },
 }
-
