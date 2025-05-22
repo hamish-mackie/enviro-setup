294b215a67ed0e2ea877dc56350563d26eb1e32d
Hamish <hamish.g.mackie@gmail.com>
2025-05-22 11:30:55 +0000

    fix lsp servers

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,24 @@
 -- treesitter.lua
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
-	opts = {
-		-- List of parsers to install
-		ensure_installed = { "cpp", "python", "yaml", "json", "cmake", "ansible", "lua_ls" },
-		sync_install = false, -- install parsers asynchronously
-		auto_install = true, -- automatically install missing parsers when entering buffer
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+        -- List of parsers to install
+        ensure_installed = { "cpp", "python", "yaml", "json", "cmake", "lua" },
+        sync_install = false, -- install parsers asynchronously
+        auto_install = true,  -- automatically install missing parsers when entering buffer
 
-		highlight = {
-			enable = true, -- false will disable the whole extension
-			additional_vim_regex_highlighting = false,
-		},
+        highlight = {
+            enable = true, -- false will disable the whole extension
+            additional_vim_regex_highlighting = false,
+        },
 
-		indent = {
-			enable = true,
-		},
-	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-	end,
+        indent = {
+            enable = true,
+        },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end,
 }
