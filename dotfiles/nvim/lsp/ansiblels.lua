4eead76b0c6aa6bdb8e20fcf8e7b23aca2112cef
hamish <hamish.g.mackie@gmail.com>
2026-03-09 21:06:01 +1100

    move dir print, fix ansibels

--- dotfiles/nvim/lsp/ansiblels.lua
+++ dotfiles/nvim/lsp/ansiblels.lua
@@ -1,21 +1,7 @@
--- ~/.config/nvim/lsp/ansiblels.lua
-
 return {
 	cmd = { "ansible-language-server", "--stdio" },
-	filetypes = { "yaml", "yml", "ansible" },
-
-	-- If root_markers is enough, prefer it (simpler and faster):
+	filetypes = { "ansible" },
 	root_markers = { "ansible.cfg", ".ansible-lint", ".git" },
-
-	-- If you specifically want the callback-style root_dir, keep this instead
-	-- (and delete root_markers above):
-	-- root_dir = function(bufnr, on_dir)
-	--   local name = vim.api.nvim_buf_get_name(bufnr)
-	--   local dir = vim.fs.dirname(name)
-	--   local found = vim.fs.find({ "ansible.cfg", ".ansible-lint", ".git" }, { path = dir, upward = true })[1]
-	--   local root = found and vim.fs.dirname(found) or vim.loop.cwd()
-	--   on_dir(root)
-	-- end,
 
 	settings = {
 		ansible = {
